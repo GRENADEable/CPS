166cb202751bbf6786c1f6f769affc9ff5a1a674
GRENADEable <kahtim.dar@gmail.com>
2022-08-22 09:54:05 +1000

    - Setting response for week 4. - Imported car audio and just setting it up in a timeline.

--- RMIT_APD/Assets/Scripts/Scriptable_Objects/DarkenData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Darken_Data")]
-public class DarkenData : ScriptableObject
-{
-    #region Public Variables
-    public float darkenLevel = default;
-    #endregion
-}
